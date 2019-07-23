af29c6351745949fb15841c787e7382eff815cdc
Prateek T <lolcrosell@gmail.com>
2019-07-23 14:42:03 -0400

    remove import button

--- edit.js
+++ edit.js
@@ -13,12 +13,8 @@ function createCanvas(){
 }
 
 var loadCanvas = function(event){
-
     var canvas = new fabric.Canvas('c');
-    var rect = new fabric.Rect({left:100, top: 150, fill: 'red', width:200, height:200});
-   // var rect = new Image("meiji.jpg");
     console.log(event.target.files[0].name);
-//    canvas.add(rect); // add object
    fabric.Image.fromURL(event.target.files[0].name, (myImg)=>{
        canvas.add(myImg);
    });
